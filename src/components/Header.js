ef62ec2672c8c178837862f6c318916520abfa7f
Odun Freeman <96088797+bornduringwar@users.noreply.github.com>
2022-03-23 09:56:11 -0400

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import "./header.css";
+export default function Header() {
+  return (
+    <header>
+      <img src="./images/logo.svg" alt="easybank logo" />
+      <img src="./images/icon-hamburger.svg" alt="nav-drop-down" />
+    </header>
+  );
+}
