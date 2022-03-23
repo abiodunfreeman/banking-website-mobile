8d0cef49e1e95b73e37ed78ab89dd72ec18bec77
Odun Freeman <96088797+bornduringwar@users.noreply.github.com>
2022-03-23 10:07:35 -0400

    added functionality to switch between hamburger icon and close icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import "./header.css";
 export default function Header() {
+  const [clicked, setClicked] = useState(false);
+  function handleClick() {
+    setClicked((prevClick) => !prevClick);
+  }
+  let icon = "";
+  clicked
+    ? (icon = "./images/icon-close.svg")
+    : (icon = "./images/icon-hamburger.svg");
   return (
     <header>
       <img src="./images/logo.svg" alt="easybank logo" />
-      <img src="./images/icon-hamburger.svg" alt="nav-drop-down" />
+      <img onClick={() => handleClick()} src={icon} alt="nav-drop-down" />
     </header>
   );
 }
