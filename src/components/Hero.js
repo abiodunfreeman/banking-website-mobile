5517cc240cbdc482970975e1a3409f4edb9ffc42
Odun Freeman <96088797+bornduringwar@users.noreply.github.com>
2022-03-23 11:10:46 -0400

    added hero description section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ export default function Hero() {
         <img
           id="hero-bg"
           src="./images/bg-intro-mobile.svg"
-          alt="phones using our easy to use mobile app"
+          alt="background for phones"
         />
         <img
           id="hero-img"
@@ -16,6 +16,12 @@ export default function Hero() {
       </div>
       <div id="hero-desc-container">
         <h1>Next generation digital banking</h1>
+        <p>
+          Take your financial life online. Your Easybank account will be a
+          one-stop-shop for spending, saving, budgeting, investing, and much
+          more.
+        </p>
+        <button className="req-btn">Request Invite</button>
       </div>
     </div>
   );
