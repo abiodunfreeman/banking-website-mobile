2a9a5a3ff0fddf45fb8a306b955415984249e299
Odun Freeman <96088797+bornduringwar@users.noreply.github.com>
2022-03-23 17:53:54 -0400

    kinda fixed proper nav bar placement had to set nav-container to position: absolute, with top prop that equals height of header

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default function App() {
   return (
     <div className="App">
       <Header toggleNav={toggleNav} />
-      <Nav />
+      {/* <Nav /> */}
       <Hero />
       <Info />
       <Articles />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./header.css";
+import Nav from "./Nav";
 export default function Header(props) {
   const [clicked, setClicked] = useState(false);
   function handleClick() {
@@ -11,9 +12,12 @@ export default function Header(props) {
     ? (icon = "./images/icon-close.svg")
     : (icon = "./images/icon-hamburger.svg");
   return (
-    <header>
-      <img src="./images/logo.svg" alt="easybank logo" />
-      <img onClick={() => handleClick()} src={icon} alt="nav-drop-down" />
-    </header>
+    <div id="header-container">
+      <header>
+        <img src="./images/logo.svg" alt="easybank logo" />
+        <img onClick={() => handleClick()} src={icon} alt="nav-drop-down" />
+      </header>
+      <Nav />
+    </div>
   );
 }
