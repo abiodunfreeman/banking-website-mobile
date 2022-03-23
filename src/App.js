22953a2cf7aa0502379fb623aed5b22e7cbe3398
Odun Freeman <96088797+bornduringwar@users.noreply.github.com>
2022-03-23 12:35:32 -0400

    fixed hero so it display, will have to do nav bar functionality at the end

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default function App() {
     <div className="App">
       <Header toggleNav={toggleNav} />
       <Nav />
-      {/* <Hero /> */}
+      <Hero />
       <Info />
     </div>
   );
